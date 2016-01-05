df74e81ed557cb1efb5ffb6d5440daecfad674ff
Mark Siemers <msiemers@quintevents.com>
2016-01-05 17:22:15 -0500

    CM-13 WIP Update category routes

--- config/routes/frontend.rb
+++ config/routes/frontend.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
     controller :frontend do
       PluginRoutes.all_locales.split("|").each do |_l|
         get "#{I18n.t("routes.group", default: "group", locale: _l)}/:post_type_id-:title" => :post_type, as: "post_type_#{_l}", constraints: {post_type_id: /[0-9]+/}
-        get "#{I18n.t("routes.category", default: "category", locale: _l)}/:category_id-:title" => :category, as: "category_#{_l}", constraints: {category_id: /[0-9]+/}
+        get "#{I18n.t("routes.category", default: "category", locale: _l)}/:category_slug" => :category, as: "category_#{_l}"
         get "#{I18n.t("routes.tag", default: "tag", locale: _l)}/:post_tag_id-:title" => :post_tag, as: "post_tag_#{_l}", constraints: {post_tag_id: /[0-9]+/}
         get "#{I18n.t("routes.profile", default: "profile", locale: _l)}/:user_id-:user_name" => :profile, constraints: {user_id: /[0-9]+/}, as: "profile_#{_l}"
         get "#{I18n.t("routes.search", default: "search", locale: _l)}" => :search, as: "search_#{_l}"
       end
 
       get 'group/:post_type_id-:title' => :post_type, as: :post_type, constraints: {post_type_id: /[0-9]+/}
-      get 'category/:category_id-:title' => :category, as: :category, constraints: {category_id: /[0-9]+/}
+      get 'category/:category_slug' => :category, as: :category
       get 'post_tag/:post_tag_id-:title' => :post_tag, as: :post_tag, constraints: {post_tag_id: /[0-9]+/}
 
       get "profile/:user_id-:user_name" => :profile, as: :profile, constraints: {user_id: /[0-9]+/}
